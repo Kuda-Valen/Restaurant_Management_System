a738b7be0f00d8d7f9ebd55eb419ff1122c0ff60
Kuda_Valen <kudamukwasi@gmail.com>
2026-07-05 23:37:09 +0200

    m

--- services/add_staff.py
+++ services/add_staff.py
@@ -1,5 +1,6 @@
 
 def add_staff():
     print("\n -- Add staff -- \n")
-    first_name = str(input("Enter Staff First Name: ")).strip()
-    last_name = str(input("Enter Staff Last Name: ")).strip()
+    first_name = input("Enter Staff First Name: ").strip()
+    last_name = input("Enter Staff Last Name: ").strip()
+    phone = input("Enter Phone: ").strip()
